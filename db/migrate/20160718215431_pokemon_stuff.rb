9f2934271471a64b4e2b61c0c40c8ade67b5b28a
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-18 16:00:35 -0600

    Updated files for pokemon

---
+++ db/migrate/20160718215431_pokemon_stuff.rb
@@ -0,0 +1,4 @@
+class PokemonStuff < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,12 +1,7 @@
 
-ActiveRecord::Schema.define(version: 20150710192918) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "pokemon", force: :cascade do |t|
-    t.string   "name"
-    t.string   "Number of Candies"
-  end
 
 end
